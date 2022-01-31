87888758d0a88f15b9510080aef8bbcfe07c6732
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-01-31 21:58:36 +0300

    Update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,39 +9,24 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('stylish format', () => {
-  const expected = `${readFile('expected-stylish-file.json')}`;
+describe.each([
+  ['stylish', 'expected-stylish-file.json'],
+  ['plain', 'expected-plain-file.json'],
+  ['json', 'expected-json-file.json'],
+])('gendiff in %s format', (format, filepath3) => {
+  const expected = readFile(filepath3);
 
   test.each([
     ['file1.json', 'file2.json'],
     ['file1.yml', 'file2.yaml'],
     ['file1.json', 'file2.yaml'],
-  ])('stylish', (filepath1, filepath2) => {
+  ])('compare %s with %s', (filepath1, filepath2) => {
     const ext1 = path.extname(filepath1);
     const ext2 = path.extname(filepath2);
     const file1 = readFile(filepath1);
     const file2 = readFile(filepath2);
     const diff = genDiffFromFiles(file1, file2, ext1, ext2);
-    const formattedDiff = formatter('stylish', diff);
-
-    expect(formattedDiff).toEqual(expected);
-  });
-});
-
-describe('plain format', () => {
-  const expected = `${readFile('expected-plain-file.json')}`;
-
-  test.each([
-    ['file1.json', 'file2.json'],
-    ['file1.yml', 'file2.yaml'],
-    ['file1.json', 'file2.yaml'],
-  ])('plain', (filepath1, filepath2) => {
-    const ext1 = path.extname(filepath1);
-    const ext2 = path.extname(filepath2);
-    const file1 = readFile(filepath1);
-    const file2 = readFile(filepath2);
-    const diff = genDiffFromFiles(file1, file2, ext1, ext2);
-    const formattedDiff = formatter('plain', diff);
+    const formattedDiff = formatter(format, diff);
 
     expect(formattedDiff).toEqual(expected);
   });
