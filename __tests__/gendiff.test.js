df4ba0800ad1127115341e7e20c00e3ca844761b
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-01-31 13:27:03 +0300

    Update tests and remove parsers.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,13 +2,14 @@ import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
 import path from 'path';
 import { genDiffFromFiles } from '../lib/gendiff.js';
+import stylish from '../lib/formatters/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('testing flat json', () => {
+test('testing json', () => {
   const expected = `${readFile('expected-file.json')}`;
   const filepath1 = 'file1.json';
   const filepath2 = 'file2.json';
@@ -17,10 +18,10 @@ test('testing flat json', () => {
   const file1 = readFile(filepath1);
   const file2 = readFile(filepath2);
 
-  expect(genDiffFromFiles(file1, file2, ext1, ext2)).toEqual(expected);
+  expect(stylish(genDiffFromFiles(file1, file2, ext1, ext2))).toEqual(expected);
 });
 
-test('testing flat yaml', () => {
+test('testing yaml', () => {
   const expected = `${readFile('expected-file.json')}`;
   const filepath1 = 'file1.yml';
   const filepath2 = 'file2.yaml';
@@ -29,10 +30,10 @@ test('testing flat yaml', () => {
   const file1 = readFile(filepath1);
   const file2 = readFile(filepath2);
 
-  expect(genDiffFromFiles(file1, file2, ext1, ext2)).toEqual(expected);
+  expect(stylish(genDiffFromFiles(file1, file2, ext1, ext2))).toEqual(expected);
 });
 
-test('testing flat mix files', () => {
+test('testing mix files', () => {
   const expected = `${readFile('expected-file.json')}`;
   const filepath1 = 'file1.yml';
   const filepath2 = 'file2.json';
@@ -41,5 +42,5 @@ test('testing flat mix files', () => {
   const file1 = readFile(filepath1);
   const file2 = readFile(filepath2);
 
-  expect(genDiffFromFiles(file1, file2, ext1, ext2)).toEqual(expected);
+  expect(stylish(genDiffFromFiles(file1, file2, ext1, ext2))).toEqual(expected);
 });
