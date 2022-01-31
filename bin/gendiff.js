9af10a1d62a6bca14948d956f5909ad2c592a866
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-01-31 13:49:19 +0300

    Update bin/gendiff.js
    gendiff.js to be bin file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,4 @@
 #!/usr/bin/env node
-import { Command } from 'commander/esm.mjs';
-import genDiff from '../lib/gendiff.js';
+import generateDifference from '../lib/commander.js';
 
-const program = new Command();
-
-program
-  .helpOption('-h, --help', 'output usage information')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --force [type]', 'output format')
-  .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
-
-program.parse();
+generateDifference();
